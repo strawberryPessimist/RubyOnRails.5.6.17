cd279d93cb7b181fd61d1ac556fbbd856b470649
Samantha Ngo <sngo1@stuy.edu>
2017-05-06 17:02:23 +0000

    finished test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'List/index'
+  get 'list/index'
 
   root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
